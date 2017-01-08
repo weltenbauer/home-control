627f66441b5516b535adbab5b0f7462a709fc1ce
Christian Rathemacher <christian@weltenbauer-se.com>
2017-01-08 23:26:59 +0100

    Switch to abgular2 and webpack

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule, ApplicationRef } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './components/app.component';
+import { ApiService } from './components/api.service';
+import { routing } from './app.routing';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    routing
+  ],
+  declarations: [
+    AppComponent
+  ],
+  providers: [
+    ApiService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+  constructor(public appRef: ApplicationRef) {}
+}
